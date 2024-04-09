46930a66441bbd34cd6b4efaa12297ec2d4f4122
gretaangebrand <ag9716@mci4me.at>
2024-04-09 07:18:21 +0200

    code von kathrin übernommen

--- load_data.py
+++ load_data.py
@@ -22,8 +22,9 @@ def load_data(file_path):
 
 
 # Daten laden und umschreiben
-data = load_data('activity.csv')
-power_W = data['PowerOriginal']
-print(power_W)
-sorted_power_W = bubble_sort(power_W)
-print(sorted_power_W[::-1])
+if __name__ == "__main__":
+    data = load_data('activity.csv')
+    power_W = data['PowerOriginal']
+    print(power_W)
+    #sorted_power_W = bubble_sort(power_W)
+    #print(sorted_power_W[::-1])
